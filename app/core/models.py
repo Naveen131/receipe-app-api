e6867923f09acb7e8b9c51a18c1b33d1a9c325ca
Naveen131 <naveen.c131@gmail.com>
2020-05-12 14:06:10 +0530

    user models and test

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self,email,password=None,**extrafields):
+        if not email:
+            raise ValueError("Users must have email")
+        user = self.model(email=self.normalize_email(email),**extrafields)
+        user.set_password(password)
+        user.save(self._db)
+        return user
+
+
+    def create_superuser(self,email,password=None,**extrafields):
+        user = self.create_user(email,password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(self._db)
+        return user
+
+class User(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(max_length=255,unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects= UserManager()
+
+    USERNAME_FIELD = "email"
